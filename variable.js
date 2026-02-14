8e6d99b485976f26054d0cd6eda4c51662b48c8c
Konda Nagendar <nagu@Kondas-MacBook-Air.local>
2026-02-15 00:07:27 +0530

    variable

---
+++ variable.js
@@ -0,0 +1,40 @@
+// let define variables
+let age;
+console.log(age); // return undeclare
+// let age = 10; this is how we assign the value to a variable on declaration
+//note we dont need to use let every time when we declare a variable
+
+//example:-
+let myage =  22;
+console.log(myage);
+//update:-
+myage = 23;
+console.log(myage);
+// dont use this bad declaration
+let x = 12;
+let y = 11;
+// good declaration
+let name = "nagu";
+console.log(name);
+
+// declare a variable with _(underscore) ($) dollar at start prefix but we cant start with number
+let game;
+let _model;
+let $car;
+//but we can't use:-
+//let 1num; -- it will not work
+
+//js is case sensitive
+let num = 10;
+let Num = 20;
+console.log(num); //10
+console.log(Num); //20
+// important to stick with a consistent naming convention superCar superBike
+
+
+
+// let, const, function, or return, as they are reserved for the language itself we cant use it
+
+//You should also avoid using special characters like exclamation points (!) or at (@) symbols, in your variable names. It is best to keep variable names readable by using letters, numbers, underscores, or dollar signs.
+
+
