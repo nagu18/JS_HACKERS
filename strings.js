5783040f07ae0d7b1f6eb8d0f800f07c5af40281
Konda Nagendar <nagu@Kondas-MacBook-Air.local>
2026-02-15 00:43:09 +0530

    update

---
+++ strings.js
@@ -0,0 +1,18 @@
+//strings are a data type like boolen ,number,..
+
+
+//non-mutable declaration
+const note = "wecome all of  u" // can declare single quotes
+const note2= 'let go'
+console.log(note);
+console.log(note2);
+
+
+
+
+//mutable string
+let  role = 'administrator';
+role = 'normal user'; // can declare by single quotes and double quotes
+console.log(role);
+
+
