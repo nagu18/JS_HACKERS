e0658a0b3fae35d69d1bbb90b723aa8d2a6a4b3a
Konda Nagendar <nagu@Kondas-MacBook-Air.local>
2026-02-15 00:26:40 +0530

    update

---
+++ let_and_const_usage.js
@@ -0,0 +1,11 @@
+//let is use for updating program tracking
+let mark; // we can declare without value
+marks = 10
+marks = 20; // updated or modify
+console.log(marks);
+
+//const is used for fixed non mutable value
+const score = 100; //const must be declare with value in it
+
+//we can use var in place of let but it not recommend it has a wider scope
+
